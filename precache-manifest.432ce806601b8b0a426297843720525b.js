2ab2a5a4b1fd1ce5f509967dbf7b19fc8ea8e4ef
robinlundgren <robinlundgrenska@gmail.com>
2020-08-11 22:46:25 +0200

    Updates

---
+++ precache-manifest.432ce806601b8b0a426297843720525b.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed843c7fab679d1cb49db18157944ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ae3694098b06658a70",
+    "url": "/static/css/main.7e5730ab.chunk.css"
+  },
+  {
+    "revision": "6299957e8d0fb745e5a2",
+    "url": "/static/js/2.d51037c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d51037c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ae3694098b06658a70",
+    "url": "/static/js/main.0f40ea7c.chunk.js"
+  },
+  {
+    "revision": "68ae16ba7553ca403aaf",
+    "url": "/static/js/runtime-main.ed3531bb.js"
+  },
+  {
+    "revision": "9825bf470314b008ca05cb80068b83f1",
+    "url": "/static/media/adobe.9825bf47.svg"
+  },
+  {
+    "revision": "23589c8543587e6fe62fb033a1b65511",
+    "url": "/static/media/angular.23589c85.svg"
+  },
+  {
+    "revision": "e14a4c1591938eb8fcf761eadbdb297f",
+    "url": "/static/media/angular_featured.e14a4c15.jpg"
+  },
+  {
+    "revision": "9573a93ba6507ecd6b90f917f6a7450f",
+    "url": "/static/media/art.9573a93b.jpg"
+  },
+  {
+    "revision": "b3439e1046d74b99912fdf429b71e391",
+    "url": "/static/media/bachelor_thesis_marcus_robin_2019.b3439e10.pdf"
+  },
+  {
+    "revision": "a18582d969dce0d7ff987d162f77a9ff",
+    "url": "/static/media/bg-mountainroad.a18582d9.jpg"
+  },
+  {
+    "revision": "87e96632f40dc93dd051ee5fb2e821ef",
+    "url": "/static/media/copy.87e96632.svg"
+  },
+  {
+    "revision": "f0c47fd20174760d0bc40637bbd8c0f0",
+    "url": "/static/media/css.f0c47fd2.svg"
+  },
+  {
+    "revision": "9d2a4bb2310c6ac1b8a741db3a648afd",
+    "url": "/static/media/education.9d2a4bb2.svg"
+  },
+  {
+    "revision": "f3a86312ccfe492a73a2d161aa623b1f",
+    "url": "/static/media/education.f3a86312.svg"
+  },
+  {
+    "revision": "2ffe822ea85d7a490cd6686a7c378456",
+    "url": "/static/media/else.2ffe822e.svg"
+  },
+  {
+    "revision": "5ca35ea0f171042c4da7bde7eeab17a3",
+    "url": "/static/media/express.5ca35ea0.svg"
+  },
+  {
+    "revision": "539696ff2c5daa0aad555ec4f1ed982c",
+    "url": "/static/media/ga.539696ff.svg"
+  },
+  {
+    "revision": "d68d3f2f36ad9e74641fd332357a95ee",
+    "url": "/static/media/github.d68d3f2f.svg"
+  },
+  {
+    "revision": "f6526dbda2c2565fac39c4f1083a28d9",
+    "url": "/static/media/graphQL.f6526dbd.svg"
+  },
+  {
+    "revision": "dcfd1a0876eddfff032954d900a5ad48",
+    "url": "/static/media/grow.dcfd1a08.svg"
+  },
+  {
+    "revision": "56fff9ebde0f49ee5e6262683527c18d",
+    "url": "/static/media/gtm.56fff9eb.svg"
+  },
+  {
+    "revision": "a794994297c061007f3f6db4fb13a1a7",
+    "url": "/static/media/home.a7949942.svg"
+  },
+  {
+    "revision": "ef2b09c0ba37d59b16e6d9292abb8c81",
+    "url": "/static/media/language.ef2b09c0.svg"
+  },
+  {
+    "revision": "6b604c07cea36e174108274e400fb86e",
+    "url": "/static/media/license.6b604c07.svg"
+  },
+  {
+    "revision": "e3b6f347bff0cea913d7d7a5130952f8",
+    "url": "/static/media/linkedin.e3b6f347.svg"
+  },
+  {
+    "revision": "1e88d37303b83e4704b41b7a3a8ae3df",
+    "url": "/static/media/mail.1e88d373.svg"
+  },
+  {
+    "revision": "8b980e33247937989d9eec9978a64b42",
+    "url": "/static/media/mail.8b980e33.svg"
+  },
+  {
+    "revision": "8e86106d6d4d1da58ff06658ab413bd8",
+    "url": "/static/media/malmberg_logo.8e86106d.png"
+  },
+  {
+    "revision": "3f6cf7f1339220d801f9246b663206a0",
+    "url": "/static/media/mini-education.3f6cf7f1.svg"
+  },
+  {
+    "revision": "ba2502dd00852fff7e4e34aa5b832370",
+    "url": "/static/media/mini-work.ba2502dd.svg"
+  },
+  {
+    "revision": "6cb06a87c458ab6b2b6c528c17f41d51",
+    "url": "/static/media/mobile-github.6cb06a87.svg"
+  },
+  {
+    "revision": "44faeb033587dbd40d8c844c27c8fe84",
+    "url": "/static/media/mobile-pdf.44faeb03.svg"
+  },
+  {
+    "revision": "f144f7d9d6b5b271941a83403eeb3b60",
+    "url": "/static/media/mobileLinkedIn.f144f7d9.svg"
+  },
+  {
+    "revision": "a3344eea14aec25d6cf2dbcfb4489888",
+    "url": "/static/media/model.a3344eea.svg"
+  },
+  {
+    "revision": "2a6a909da27347bb8499ad3d94557d1e",
+    "url": "/static/media/mongoDB.2a6a909d.svg"
+  },
+  {
+    "revision": "f5ae336431755c6fb70fe02d437e7ef4",
+    "url": "/static/media/mongo_features.f5ae3364.jpg"
+  },
+  {
+    "revision": "c508b582409f0dce132473ff4ad94f8d",
+    "url": "/static/media/node.c508b582.svg"
+  },
+  {
+    "revision": "ee6eeb6fbf597b5b157353bbe2af4009",
+    "url": "/static/media/node_features.ee6eeb6f.png"
+  },
+  {
+    "revision": "881f1e959b902d8aaeaf3b9f3f32e43a",
+    "url": "/static/media/ocean.881f1e95.mp4"
+  },
+  {
+    "revision": "a954230604290e18aeb7377500faed07",
+    "url": "/static/media/pdf.a9542306.svg"
+  },
+  {
+    "revision": "0cb62d1f95d375a98dc6bc13ad67f9aa",
+    "url": "/static/media/phone.0cb62d1f.svg"
+  },
+  {
+    "revision": "faeb59fc90bd227077d8a4a5503170f2",
+    "url": "/static/media/phone.faeb59fc.svg"
+  },
+  {
+    "revision": "9bf51505109d19795a99822b1b7491c1",
+    "url": "/static/media/pinpoint.9bf51505.svg"
+  },
+  {
+    "revision": "9de66b83e6043ff17ad3a5b95b9990dc",
+    "url": "/static/media/react.9de66b83.svg"
+  },
+  {
+    "revision": "c76a4c6741e72b3d58f23856ce045e22",
+    "url": "/static/media/react.c76a4c67.svg"
+  },
+  {
+    "revision": "b26b8cc4beaacef97e394c537eec4752",
+    "url": "/static/media/react_cv_img.b26b8cc4.jpg"
+  },
+  {
+    "revision": "461013a505c849fc902f15bc8f47c0d4",
+    "url": "/static/media/redux.461013a5.svg"
+  },
+  {
+    "revision": "cb657286d02a51b9b80aa1f50a180ee4",
+    "url": "/static/media/robin_lundgren.cb657286.jpg"
+  },
+  {
+    "revision": "202564c021aab85fae1ae4da8d8495af",
+    "url": "/static/media/seo.202564c0.svg"
+  },
+  {
+    "revision": "04e43eeb10fbc1e14ea9d12945bdd7a1",
+    "url": "/static/media/sql.04e43eeb.svg"
+  },
+  {
+    "revision": "32815377d1d1dc8421aef6f6c4835ccd",
+    "url": "/static/media/wordpress.32815377.svg"
+  },
+  {
+    "revision": "196b52f68bbed59f87717d7c8d9f89af",
+    "url": "/static/media/work.196b52f6.svg"
+  },
+  {
+    "revision": "f8b962e20a388b849d229114211513ac",
+    "url": "/static/media/work.f8b962e2.svg"
+  }
+]);
